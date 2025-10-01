1966ba8b1cefb067860b9f570626e79b0aca96e8
Zan <99092011+Zanixpy@users.noreply.github.com>
2025-10-01 13:51:49 +0000

    non

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -19,9 +19,7 @@ unsigned int    ft_strlcpy(char *dst, char *src, unsigned int n)
 {
     int i;
     int j;
-
-    if (n == 0)
-        return (lenL(src));
+    
     i = 0;
     j = lenL(dst);
     while (src[i] && (j + i) < (n - 1))
@@ -30,17 +28,16 @@ unsigned int    ft_strlcpy(char *dst, char *src, unsigned int n)
         i++;
     }
     dst[j + i] = '\0';
-    return (j + lenL(src));
+    return (lenL(src));
 }
 
 int main(void)
 {
-    char *src = "Hey";
+    char *src = "Heqvvvd";
     char dst[10];
     dst[0] = 'A';
     dst[1] = 'b';
-    dst[2] = 'c';
-    dst[3] = '\0';
-    printf("%d, %ld\n", ft_strlcpy(dst, src, 2), strlcpy(dst, src, 3));
+    dst[2] = '\0';
+    printf("%d, %ld\n", ft_strlcpy(dst, src, 7), strlcpy(dst, src, 7));
     return (0);
 }
