b582e381eeb37751db5f720cbd20a4dc9a750140
Zan <99092011+Zanixpy@users.noreply.github.com>
2025-10-10 13:58:46 +0000

    go home

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -17,3 +17,31 @@ void ft_lstclear(t_list **lst, void (*del)(void*))
     }
     *lst = 0;    
 }
+
+
+/*void proto(void *p)
+{
+    int *s = (int *) p;
+    s = 0;
+    (void)s;
+}
+
+int main(void)
+{
+    int a = 42;
+    int b = 12;
+    int c = 55654;
+    t_list *p = ft_lstnew((void *)&a);
+    ft_lstadd_front(&p, ft_lstnew((void *)&b));
+    ft_lstadd_back(&p, ft_lstnew((void *)&c));
+    ft_lstclear(&p, &proto);
+    if (p == 0)
+    {
+        printf("Done\n");
+    }else
+        printf("Rater\n");
+    //printf("%d\n", * (int *) (p->next)->next->content);
+    //printf("%d\n", * (int *) (p->next)->content);
+    //printf("%d\n", * (int *) p->content);  
+    return (0);
+}*/
