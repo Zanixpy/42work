91f93a30b09d117cc484e1e225da4664bf0f2a78
Onyx Schadrac Mawele <osm@MacBook-Air-de-Onyx.local>
2025-10-14 20:55:45 +0200

    trying smth

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,19 +1,37 @@
 #include "get_next_line.h"
+#include <unistd.h>
+#include <fcntl.h>
 
 char *get_next_line(int fd)
 {
-    static char buf;
-    int i;
-    int n;
+    char *buf;
+    static int read_bytes;
+    //int n;
 
-    i = 0;
     if (fd < 0 )
         return (0);
-    while (read(fd,buf, BUFFER_SIZE) != 0)
+    buf = (char *) malloc(1 * sizeof(char));
+    if (buf == 0)
+        return (0);
+    while ((read_bytes = read(fd, buf,  1)) > 0)
     {
+        if (buf == 0)
+            return (0);
+        /*if (buf[0] == '\n' || buf[0] == '\0')
+            return (0);*/
+        write(1, buf, 1);
+        free(buf);
+        buf = (char *) malloc(1 * sizeof(char));
     }
-    
-    
-    
+    if (read_bytes == 0 || read_bytes < 0)
+        return (0);
+    return ("Test");    
+}
+
+int main(void)
+{
+    int fd = open("text.txt", O_RDWR);
+    while (get_next_line(fd) != 0);    
+    close(fd);
     return (0);
 }
