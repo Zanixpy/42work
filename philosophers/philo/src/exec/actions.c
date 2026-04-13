b6e4124b49461e5b00e7489b9d3dfe7868828b71
Onyx Schadrac Mawele <omawele@f4r1s8.clusters.42paris.fr>
2026-04-13 17:06:42 +0200

    little fix

--- philosophers/philo/src/exec/actions.c
+++ philosophers/philo/src/exec/actions.c
@@ -6,7 +6,7 @@
 /*   By: omawele <omawele@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/16 16:12:50 by omawele           #+#    #+#             */
-/*   Updated: 2026/04/13 16:53:23 by omawele          ###   ########.fr       */
+/*   Updated: 2026/04/13 17:06:25 by omawele          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ void	eat(t_philo *philo)
 	if (philo->eat_count == philo->data.eat_count)
 	{
 		pthread_mutex_unlock(&philo->lock_eat_count);
-		return;
-	}		
+		return ;
+	}
 	pthread_mutex_unlock(&philo->lock_eat_count);
 	print_state(philo, philo->index, "is eating");
 	pthread_mutex_lock(&philo->lock_last_meal);
